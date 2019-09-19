7956945f26b334fd9f1073e0ba08ca15d84ba79e
raysilent7 <rgc471@hotmail.com>
2019-09-19 12:03:53 -0300

    formGroup to control signup form

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      type : ['1', [Validators.required]],
+      cpfOrCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      password : ['123', [Validators.required]],
+      location : ['Rua Via', [Validators.required]],
+      number : ['25', [Validators.required]],
+      complement : ['Apto 3', []],
+      neighbr : ['Copacabana', []],
+      cep : ['10828333', [Validators.required]],
+      phone1 : ['977261827', [Validators.required]],
+      phone2 : ['', []],
+      phone3 : ['', []],
+      stateId : [null, [Validators.required]],
+      cityId : [null, [Validators.required]]
+    });
+  }
 
   signupUser() {
-    console.log('morreu');
+    console.log('conta criada com sucesso');
   }
 
   ngOnInit() {
