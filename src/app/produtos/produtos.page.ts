648511dc302440dec0e2f13d06904281bc746218
raysilent7 <rgc471@hotmail.com>
2019-11-14 12:11:07 -0300

    loading product imgs

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProdutoDTO} from "../../models/produto.DTO";
 import {ProdutoService} from "../../services/domain/produto.service";
 import {ActivatedRoute} from "@angular/router";
+import {API_CONFIG} from "../../config/api.config";
 
 @Component({
   selector: 'app-produtos',
@@ -16,25 +17,34 @@ export class ProdutosPage implements OnInit {
               public route: ActivatedRoute){
   }
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(parametros => {if (parametros['categoria']) {
-      let cat_id = parametros['categoria'];
-      console.log('passou por aqui tbm');
-      console.log(cat_id);
-      this.produtoService.findByCategoria(cat_id)
-          .subscribe(response => {
-                this.items = response['content'];
+  loadImageUrls () {
+      for (var i=0; i<this.items.length; i++) {
+          let item = this.items[i];
+          this.produtoService.getSmallImageFromBucket(item.id)
+              .subscribe(response => {
+                  item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
               },
               error => {});
-    }
-    else {
-        console.log('passou por aqui');
-        this.produtoService.findAll()
-            .subscribe(response => {
-                    this.items = (<ProdutoDTO[]>response);
-                },
-                error => {});
-    }});
+      }
   }
 
+  ngOnInit() {
+      this.route.queryParams.subscribe(parametros => {if (parametros['categoria']) {
+          let cat_id = parametros['categoria'];
+          this.produtoService.findByCategoria(cat_id)
+              .subscribe(response => {
+                  this.items = response['content'];
+                  this.loadImageUrls();
+              },
+              error => {});
+        }
+      else {
+          this.produtoService.findAll()
+              .subscribe(response => {
+                  this.items = (<ProdutoDTO[]>response);
+                  this.loadImageUrls();
+              },
+              error => {});
+      }});
+  }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {API_CONFIG} from '../../config/api.config';
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ProdutoService {
@@ -8,11 +9,16 @@ export class ProdutoService {
     constructor(public http: HttpClient) {
     }
 
-    findByCategoria(categoria_id : string) {
+    findByCategoria (categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/products/categories?categories=${categoria_id}`);
     }
 
     findAll () {
         return this.http.get(`${API_CONFIG.baseUrl}/products`);
+    }
+
+    getSmallImageFromBucket (id: string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType: 'blob'});
     }
 }
